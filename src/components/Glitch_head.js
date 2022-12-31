f60087d1fbdfe80c0f933f336a0eb6819a176a33
v-rana <63035799+v-rana@users.noreply.github.com>
2022-12-31 15:32:25 +0530

    defining the engines
    cannot deploy, so lets change the some package

--- src/components/Glitch_head.js
+++ src/components/Glitch_head.js
@@ -3,7 +3,7 @@ import './css/Glitch_head.css'
 function Glitch_head() {
   return (
     <div>
-      <h1 className="glitch text-white bg-black">
+      <h1 className="glitch text-black bg-white">
       <span >Weather App</span>
       <span>Weather App</span>
       <span >Weather App</span>
